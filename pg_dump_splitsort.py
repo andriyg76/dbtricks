4e62da4689656a172f726055b91e32d692eba84c
Andriy Gushuley <agushuley@me.com>
2014-10-14 23:45:25 +0300

    Refactored pg_split_sort

--- pg_dump_splitsort.py
+++ pg_dump_splitsort.py
@@ -9,10 +9,7 @@ import tempfile
 import importer
 
 
-COPY_RE = re.compile(r'COPY .*? \(.*?\) FROM stdin;\n$')
-
-
-def try_float(s):
+def _try_as_float(s):
     if not s or s[0] not in '0123456789.-':
         # optimization
         return s
@@ -22,24 +19,210 @@ def try_float(s):
         return s
 
 
-def lines_compare(l1, l2):
+def _lines_compare(l1, l2):
     p1 = l1.split('\t', 1)
     p2 = l2.split('\t', 1)
-    result = cmp(try_float(p1[0]), try_float(p2[0]))
+    result = cmp(_try_as_float(p1[0]), _try_as_float(p2[0]))
     if not result and len(p1) > 1 and len(p2) > 1:
-        return lines_compare(p1[1], p2[1])
+        return _lines_compare(_try_as_float(p1[1]), _try_as_float(p2[1]))
     return result
 
-
-DATA_COMMENT_RE = r'-- Data for Name: (?P<table>.*?); Type: TABLE DATA; Schema: (?P<schema>.*?);'
-END_COPY_LINE = '\\.\n'
 
 def key(line):
     keys = line.split('\t')
     if len(keys) >= 2:
-        return try_float(keys[0]), try_float(keys[1])
+        return _try_as_float(keys[0]), _try_as_float(keys[1])
     else:
-        return try_float(keys[0]), None
+        return _try_as_float(keys[0]), None
+
+
+class _Dumper:
+    def __init__(self):
+        self._output = None
+        self._buf = []
+
+    def flush(self):
+        if not self._output:
+            raise ValueError("Output is no opened during flush")
+        self._output.writelines(self._buf)
+        self._output.flush()
+        self._buf = []
+
+    def new_output(self, path):
+        if self._output:
+            self.flush()
+            self._output.close()
+        self._output = file(path, 'w')
+
+    def pop_last_lines(self, lines=1):
+        accum = []
+        if lines > 0:
+            while lines > 0 and len(self._buf) > 0:
+                accum.append(self._buf.pop())
+                lines -= 1
+            accum.reverse()
+        return accum
+
+    def add_lines(self, lines):
+        self._buf.extend(lines)
+
+    def append(self, line):
+        self._buf.append(line)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._output:
+            self.flush()
+            self._output.close()
+            self._output = None
+
+# class _Dumper
+
+
+_END_COPY_LINE = '\\.\n'
+
+
+class _DataHandler:
+    def __init__(self, max_chunk_size, start_line, table_name, counter):
+        self._max_chunk_size = max_chunk_size
+
+        self._start_line = start_line
+        self._table_name = table_name
+        self._counter = counter
+
+        self._buf = []
+        self._buf_size = 0
+        self._chunks = []
+        self._flushed = False
+
+    def add_line(self, line):
+        if self._flushed:
+            raise ValueError("Can't add table data, table interaction already flushed")
+        self._buf.append(line)
+        self._buf_size += len(line)
+        if self._buf_size > self._max_chunk_size:
+            importer.verbose("Splitting %s temporary data, %d-th part, %d lines, size %d" %
+                             (self._table_name, len(self._chunks) + 1, len(self._buf), self._buf_size))
+
+            self._buf.sort(cmp=_lines_compare)
+            chunk = tempfile.TemporaryFile("r+w")
+            chunk.writelines(self._buf)
+            chunk.seek(0)
+
+            self._chunks.append(chunk)
+            self._buf_size = 0
+            self._buf = []
+
+    # add_line
+
+    def flush_data(self, dumper):
+        if self._flushed:
+            raise ValueError("Can't add table data, table interaction already flushed")
+        importer.verbose("Storing %s data, %d lines in memory, size %d" %
+                         (self._table_name, len(self._buf), self._buf_size))
+        self._buf.sort(cmp=_lines_compare)
+
+        if len(self._chunks):
+
+            # multiple chunks
+            sequence = 1
+            dumper.append(self._start_line)
+            output_size = 0
+            sorted_memory = [(key(line), line) for line in self._buf]
+            sorted_chunks = [[(key(line), line) for line in chunk]
+                             for chunk in self._chunks]
+            for _key, _line in heapq.merge(sorted_memory, *sorted_chunks):
+                dumper.append(_line)
+                output_size += len(_line)
+                if output_size > self._max_chunk_size:
+                    dumper.append(_END_COPY_LINE)
+                    dumper.flush()
+
+                    dumper.new_output('{counter:04}_{table_name}_{sequence:05}.sql'.format(
+                        counter=self._counter, table_name=self._table_name, sequence=sequence))
+                    output_size = 0
+                    sequence += 1
+
+                    dumper.append(self._start_line)
+                    # for _key _ine in sorted data
+        else:
+            dumper.append(self._start_line)
+            dumper.add_lines(self._buf)
+            dumper.append(_END_COPY_LINE)
+            dumper.flush()
+
+        dumper.append(_END_COPY_LINE)
+        dumper.flush()
+
+        for chunk in self._chunks:
+            chunk.close()
+        self._chunks = []
+        self._buf_size = 0
+        self._buf = []
+
+        self._flushed = True
+        # flush_data
+
+# class _DataHandler
+
+
+COPY_RE = re.compile(r'COPY .*? \(.*?\) FROM stdin;\n$')
+DATA_COMMENT_RE = r'-- Data for Name: (?P<table>.*?); Type: TABLE DATA; Schema: (?P<schema>.*?);'
+CONSTRAINT_COMMENT_RE = r'-- Name: .*; Type: (.*CONSTRAINT|INDEX); Schema: '
+
+
+def __do_split(args, sql_dump_file, order):
+    with _Dumper() as dumper:
+        counter = 0
+        previous_table = None
+        dumper.new_output('0000_prologue.sql')
+        table_name = None
+        epilogue = False
+        data_handler = None
+
+        for line in sql_dump_file:
+            if data_handler:
+                if line == _END_COPY_LINE:
+                    data_handler.flush_data(dumper)
+                    data_handler = None
+                    previous_table = table_name
+                    counter = 0
+                else:
+                    data_handler.add_line(line)
+            else:  # not data_handler.is_data
+                if epilogue or line in ('\n', '--\n'):
+                    dumper.append(line)
+                elif re.match(CONSTRAINT_COMMENT_RE, line):
+                    backup = dumper.pop_last_lines()
+                    dumper.flush()
+                    dumper.new_output('9999_epilogue.sql')
+                    dumper.add_lines(backup)
+                    dumper.append(line)
+                    epilogue = True
+                else:
+                    matcher = re.match(DATA_COMMENT_RE, line)
+                    if matcher:
+                        table_name = '{schema}.{table}'.format(
+                            schema=matcher.groupdict()['schema'], table=matcher.groupdict()['table'])
+                        counter = importer.get_order_number(order, table_name, previous_table)
+
+                        backup = dumper.pop_last_lines()
+                        dumper.flush()
+                        dumper.new_output('{counter:04}_{table_name}.sql'.format(
+                            counter=counter, table_name=table_name))
+                        dumper.add_lines(backup)
+                        dumper.append(line)
+                    elif table_name and COPY_RE.match(line):
+                        data_handler = _DataHandler(args.chunk_size, line, table_name, counter)
+                    else:
+                        dumper.append(line)
+
+                        # for every line
+
+
+# def __do_import
 
 
 def split_sql_file(args):
@@ -63,137 +246,20 @@ def split_sql_file(args):
         if os.path.exists(".order"):
             with open('.order', 'r') as infile:
                 order = json.load(infile)
-            importer.verbose("Read .order file", order)
+                importer.verbose("Read .order file", order)
         else:
             importer.verbose("Can't find .order file, starting from scratch")
             order = {}
-        # if .order exist
+            # if .order exist
 
-        output = None
-        buf = []
+        __do_split(args, sql_dump_file, order)
 
-        def flush():
-            output.writelines(buf)
-            buf[:] = []
-
-        def new_output(path):
-            if output:
-                output.close()
-            return file(path, 'w')
-
-        copy_lines = None
-        copy_line = None
-        copy_size = 0
-        counter = 0
-        output = new_output('0000_prologue.sql')
-        table_name = None
-
-        for line in sql_dump_file:
-            if copy_lines is None:
-                if line in ('\n', '--\n'):
-                    buf.append(line)
-                elif line.startswith('SET search_path = '):
-                    flush()
-                    buf.append(line)
-                else:
-                    matcher = re.match(DATA_COMMENT_RE, line)
-                    if matcher:
-                        schema = matcher.groupdict()['schema']
-                        table = matcher.groupdict()['table']
-                        previous_table = table_name
-                        table_name = '{schema}.{table}'.format(schema=schema,table=table)
-                        counter = importer.get_order_number(order, table_name, previous_table)
-                        output = new_output(
-                            '{counter:04}_{schema}.{table}.sql'.format(
-                                counter=counter,
-                                schema=schema,
-                                table=table))
-                        buf.append(line)
-                    elif COPY_RE.match(line):
-                        copy_line = line
-                        copy_lines = []
-                        chunks = []
-                        copy_size = 0
-                    else:
-                        output = new_output('9999_epilogue.sql')
-                        buf.append(line)
-                    flush()
-            else:  # if copy_lines
-                if line == END_COPY_LINE:
-                    copy_lines.sort(cmp=lines_compare)
-                    if not len(chunks):
-                        importer.verbose("Storing %d lines directly to %s.%s table output" %
-                                         (len(copy_lines), schema, table))
-                        buf.append(copy_line)
-                        buf.extend(copy_lines)
-                        buf.append(END_COPY_LINE)
-                        flush()
-                    else:
-                        if len(copy_lines):
-                            importer.verbose("Storing %d lines to last, %d-th chunk" %
-                                             (len(copy_lines), len(chunks) + 1))
-                            temp_file = tempfile.TemporaryFile("r+w")
-                            temp_file.writelines(copy_lines)
-                            temp_file.seek(0)
-                            chunks.append(temp_file)
-
-                        importer.verbose("Merging %d chunks of table %s.%s" %
-                                         (len(chunks), schema, table))
-
-                        buf.append(copy_line)
-
-                        output_size = 0
-                        sequence = 2
-                        for _key, _line in heapq.merge(*[
-                            [(key(line), line) for line in chunk]
-                            for chunk in chunks]):
-                            buf.append(_line)
-                            output_size += len(_line)
-                            if output_size > args.chunk_size:
-                                buf.append(END_COPY_LINE)
-                                flush()
-                                output = new_output(
-                                    '{counter:04}_{schema}.{table}_{sequence:05}.sql'.format(
-                                        counter=counter,
-                                        schema=schema,
-                                        table=table,
-                                        sequence=sequence))
-
-                                sequence += 1
-                                output_size = 0
-                                buf.append(copy_line)
-                                # if chunk should be flushed
-                        # for key, line in all_lines
-                        buf.append(END_COPY_LINE)
-                        flush()
-
-                    # if not chunks
-
-                    copy_lines = None
-                    copy_size = 0
-                    copy_line = None
-                    chunks = []
-                else:
-                    copy_lines.append(line)
-                    copy_size += len(line)
-                    if copy_size > args.chunk_size:
-                        importer.verbose("Storing %d lines to %d-th chunk" %
-                                         (len(copy_lines), len(chunks) + 1))
-                        temp_file = tempfile.TemporaryFile("r+w")
-                        copy_lines.sort(cmp=lines_compare)
-                        temp_file.writelines(copy_lines)
-                        temp_file.seek(0)
-                        chunks.append(temp_file)
-                        copy_size = 0
-                        copy_lines = []
-                # if line = END_COPY_LINE (end table data)
-            # copy_lines is none
-        # for every line
         importer.verbose("Writing updated .order file")
         with open('.order', 'w') as outfile:
             json.dump(order, fp=outfile, sort_keys=True, indent=4, separators=(',', ': '))
-    # with sql_dump_file
-# split_sql_file()
+            # with sql_dump_file
+
+# def split_sql_file
 
 if __name__ == '__main__':
     split_sql_file(importer.create_argsparser())
